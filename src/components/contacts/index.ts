13610d6eed5468f7f0d717e62f2383d3e42a905c
kafulu25 <kafuluwabamanyi@gmail.com>
2026-06-19 04:50:12 +0300

    Update index.ts

--- src/components/contacts/index.ts
+++ src/components/contacts/index.ts
@@ -1,1 +1,4 @@
+// src/components/contacts/index.ts
 export { ContactsPanel } from './ContactsPanel';
+// If you have other exports like ContactList, add them here.
+// But DO NOT include PhoneScanPanel – that belongs to the phoneScan folder.
